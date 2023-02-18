12ecc34890539de9d263288ca72e2a2e901b7fe0
simonox <oliver.ochs@information-architects.de>
2023-02-18 17:20:13 +0100

    gitignore

---
+++ software/firmware/energy-montior/energy-monitor/energy-monitor.ino
@@ -0,0 +1,26 @@
+/*
+SCT-013 Sensor - Power meassurement, based on Thomas Edlinger's code for "www.edistechlab.com"
+Required libraries (Tools -> manage libraries)
+ - EmonLib libary V1.1.0 by OpenEnergyMonitor
+Based on EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+*/
+
+#include "EmonLib.h"
+EnergyMonitor emon1;
+const byte current1Pin = A0; // ADC-PIN
+const byte voltage = 230;  // Power voltage in Europe = 230 V
+
+void setup() {
+  Serial.begin(115200);
+  analogReadResolution(ADC_BITS); // activate 12 Bit resolution for our ESP32
+  emon1.current(current1Pin, 4);  // Pin and Calibration
+}
+
+void loop() {
+  double Irms = emon1.calcIrms(1480);
+  Serial.print(Irms*voltage);
+  Serial.print(" Watt  -  ");
+  Serial.print(Irms);
+  Serial.println(" Ampere");
+  delay(1000);
+}
