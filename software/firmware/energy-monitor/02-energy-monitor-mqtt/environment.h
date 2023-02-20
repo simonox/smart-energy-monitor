0e9b521d2ae58c4fb42fdd274342de3cd9b2bf36
simonox <oliver.ochs@information-architects.de>
2023-02-20 16:40:23 +0100

    fixed typo

---
+++ software/firmware/energy-monitor/02-energy-monitor-mqtt/environment.h
@@ -0,0 +1,6 @@
+// Replace with your network credentials
+#define secrect_ssid "Guest"
+#define secret_password "guestguest"
+#define mqtt_server "192.168.2.103"
+#define mqtt_port 1883
+#define mqtt_prefix "/iot-platform/engergy-montitor/test-device"
