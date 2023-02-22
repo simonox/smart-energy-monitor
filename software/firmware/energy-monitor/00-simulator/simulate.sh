8065beb583e8083ac53e41c1227a4646a9039897
simonox <oliver.ochs@information-architects.de>
2023-02-22 15:44:07 +0100

    typo

--- software/firmware/energy-monitor/00-simulator/simulate.sh
+++ software/firmware/energy-monitor/00-simulator/simulate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Publishing random values to topic '/iot-platform/energy-monitor/test-device‘
+echo Publishing random values to topic '/iot-platform/energy-monitor/test-device'
 while true  
 do
     mosquitto_pub -h localhost -p 1883 -t '/iot-platform/energy-monitor/test-device/watt' -m  $((1 + $RANDOM % 10 * 500))
