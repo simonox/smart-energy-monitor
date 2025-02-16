64660d158810080a6725e857b58163c5fcb32c67
Oliver Ochs <oliver.ochs@information-architects.de>
2025-02-16 12:33:22 +0100

    Update environment.h

--- software/energy-monitor/02-energy-monitor-mqtt/environment.h
+++ software/energy-monitor/02-energy-monitor-mqtt/environment.h
@@ -1,7 +1,7 @@
 // Replace with your network credentials
 
 #define secrect_ssid "Guest"
-#define secret_password "guestguest"
+#define secret_password "guestguest" #this is just an example
 #define mqtt_server "192.168.1.233"
 #define mqtt_port 1883
 #define mqtt_prefix "/iot-platform/energy-monitor/test-device"
